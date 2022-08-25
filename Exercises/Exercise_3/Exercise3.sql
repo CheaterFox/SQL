bf315dbe017b8b92f58d09ac0edfd5eee84f397d
Muhammed Said ÖĞÜT <msaid.1996@hotmail.com>
2022-08-25 19:26:54 +0300

    Exercise 3 updated

--- Exercises/Exercise_3/Exercise3.sql
+++ Exercises/Exercise_3/Exercise3.sql
@@ -2,7 +2,7 @@ SELECT country FROM country WHERE country LIKE 'A%a';
 
 SELECT country FROM country WHERE country LIKE '_____%n';
 
-SELECT title FROM film WHERE title LIKE '%t%t%t%t%';
+SELECT title FROM film WHERE title ILIKE '%t%t%t%t%';
 
 SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
 
